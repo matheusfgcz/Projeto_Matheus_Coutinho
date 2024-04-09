c292a3d86527b62a8b6bec90d5c9bd3fe98f8a31
Matheus Felipe Godoi Coutinho <138553871+matheusfgcz@users.noreply.github.com>
2024-04-09 20:11:45 -0300

    Criada nova classe D
    Criada nova classe D

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D {
+  private double D1;
+  private double D2;
+
+  public void MD1() {
+    System.out.println("MD1");
+  }
+
+  public void MD2() {
+    System.out.println("MD2");
+  }
+}
